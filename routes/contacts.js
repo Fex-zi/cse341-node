3633275b9486f32ab3a5df7f884e58ac2d5f2fca
Fex-zi <fexzi87@gmail.com>
2023-09-15 18:45:06 -0600

    week-01

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/contact'); // Import your Contact model
+const Contact = require('../models/contact'); // Import Contact model
 
 // GET all contacts
 router.get('/', async (req, res) => {
